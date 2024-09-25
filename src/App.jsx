a3051949c372f47608b553376d75c6499b1a7d6a
Ahbideen Yusuf <ahbideeny@gmail.com>
2024-09-25 19:17:40 +0100

    Remove redundant div and style

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,41 @@ import "./App.css";
 import StoryQueue from "./components/StoryQueue";
 import TimerArrow from "./components/TimerArrow";
 import { TimerArrowProvider } from "./components/TimerArrowProvider";
-import AppLogo from "./components/AppLogo";
+import Edge from "./components/Edge";
 import Time from "./components/Time";
 import Button from "./components/Button";
-// import Timer from "./components/Timer";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-body">
-        <div className="App-header">
-          <AppLogo
-            className="App-logo"
-            src="/assets/Edge_Emotional_States_Sitting_Stool_Blue.svg"
-            alt="Edge"
-          />
-
-          <div className="App-header-div time-pending">
+    <main className="main-container">
+      <section className="top-section">
+        {/* <Edge
+          className="App-logo"
+          src="/assets/Edge_Emotional_States_Sitting_Stool_Blue.svg"
+          alt="Edge"
+        />
+        <div className="time-group">
+          <div>
             <Time value="4:59" className="time" />
             <Time value="PENDING" className="pending" />
           </div>
-
-          <TimerArrowProvider>
-            <TimerArrow className="arrow" />
-          </TimerArrowProvider>
-
-          <div className="App-header-div">
-            <div className="black-div"></div>
-            <Button value="START" className="confirm-btn start" />
-          </div>
-
-          <div className="App-header-div-two">
-            <AppLogo src="/assets/Logo.svg" alt="Logo" />
-            <Button value="PAUSE" className="confirm-btn pause" />
-          </div>
+        <TimerArrowProvider>
+          <TimerArrow className="arrow" />
+        </TimerArrowProvider>
         </div>
-        <StoryQueue />
-      </div>
-    </div>
+        <div>
+          <Edge src="/assets/Logo.svg" alt="Logo" />
+          <div className="btn-top">
+            <Button label="START" className="confirm-btn start" />
+            <Button label="PAUSE" className="confirm-btn pause" />
+          </div>
+        </div> */}
+      </section>
+      <section className="mid-section">
+        {/* <StoryQueue /> */}
+      </section>
+      <section className="bottom-section"></section>
+    </main>
   );
 }
 
