a0bdaaf081be127a2c0aa937eee77fe1cd9ffe84
Mohammed Najeem <mohammednajeem18@gmail.com>
2024-09-26 18:29:43 +0100

    Add sticky arrows component, corret radiobutton name

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,44 @@ import "./App.css";
 import StoryQueue from "./components/StoryQueue";
 import TimerArrow from "./components/TimerArrow";
 import { TimerArrowProvider } from "./components/TimerArrowProvider";
-import Edge from "./components/Edge";
+// import AppLogo from "./components/AppLogo";
 import Time from "./components/Time";
 import Button from "./components/Button";
-import Ring from "./components/Ring";
-import LowerPointerArrow from "./components/LowerPointerArrow";
-import TalkBubble from "./components/TalkBubble";
-import EdgeStanding from "./components/EdgeStanding";
-
 // import Timer from "./components/Timer";
 
 function App() {
   return (
-    <main className="main-container">
-      <section className="top-section">
-        <Edge
-          className="App-logo"
-          src="/assets/Edge_Emotional_States_Sitting_Stool_Blue.svg"
-          alt="Edge"
-        />
-        <div className="time-group">
-          <div>
+    <div className="App">
+      <div className="App-body">
+        <div className="App-header">
+          {/* <AppLogo
+            className="App-logo"
+            src="/assets/Edge_Emotional_States_Sitting_Stool_Blue.svg"
+            alt="Edge"
+          /> */}
+
+          <div className="App-header-div time-pending">
             <Time value="4:59" className="time" />
             <Time value="PENDING" className="pending" />
           </div>
+
           <TimerArrowProvider>
             <TimerArrow className="arrow" />
           </TimerArrowProvider>
-        </div>
-        <div>
-          <Edge src="/assets/Logo.svg" alt="Logo" />
-          <div className="btn-top">
-            <Button label="START" className="confirm-btn start" />
-            <Button label="PAUSE" className="confirm-btn pause" />
+
+          <div className="App-header-div">
+            <div className="black-div"></div>
+            <Button value="START" className="confirm-btn start" />
+          </div>
+
+          <div className="App-header-div-two">
+            {/* <AppLogo src="/assets/Logo.svg" alt="Logo" /> */}
+            <Button value="PAUSE" className="confirm-btn pause" />
           </div>
         </div>
         <StoryQueue />
-
-      </section>
-    </main>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/StickyArrow.jsx
@@ -0,0 +1,5 @@
+function StickyArrow() {
+  return <img src="/assets/StickyArrow.png" />;
+}
+
+export default StickyArrow;
