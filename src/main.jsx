58643e9a795ff9b6eab74d87caf7a284b4b5c884
Ahbideen Yusuf <ahbideeny@gmail.com>
2024-10-21 16:44:28 +0100

    Remove bootstrap because of conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./css/loveSroll.css"
-// import "bootstrap/dist/css/bootstrap.css";
+import "./css/loveSroll.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-); 
+);
