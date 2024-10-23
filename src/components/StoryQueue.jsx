db9671a666f28117e72b6dcff73e5d2b3e1b9dda
Saysamiche <140890694+Saysamiche@users.noreply.github.com>
2024-10-23 14:18:38 +0100

    Added an onclick event for buttons and console log the result😉

--- src/components/StoryQueue.jsx
+++ src/components/StoryQueue.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "../css/storyQueue.css";
 
-const StoryQueue = () => {
+const StoryQueue = ({children}) => {
   return (
     <div className="story-queue">
-      <p>
+      {children}
+      {/* <p>
         Eating food is often a personal pleasure and social activity. Some
         people like all types of ....
-      </p>
+      </p> */}
     </div>
   );
 };
