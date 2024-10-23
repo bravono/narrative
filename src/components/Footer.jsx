bc30d1e7b85f4722ac3f6b1270503e1d7c4a71c9
Ahbideen Yusuf <ahbideeny@gmail.com>
2024-10-23 11:21:25 +0100

    Refactor Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,5 @@
-import "../css/Footer.css";
-import Button from "./Button";
+import BottomButton from "./BottomButton";
 
 export default function Footer() {
-  return (
-    <div className="bottom-buttons">
-      <div className="btn-btm-first-row">
-        <Button
-          label="COMPARE"
-          className="button"
-          color="color-variation-five"
-        />
-        <Button label="TALK" className="button" color="color-variation-five" />
-      </div>
-      <div className="btn-btm-second-row">
-        <Button
-          label="PDF IT"
-          className="button"
-          color="color-variation-five"
-        />
-        <Button label="EXIT" className="button" color="color-variation-five" />
-      </div>
-    </div>
-  );
+  return <BottomButton />;
 }
