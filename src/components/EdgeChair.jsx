75909fbff9600f41ef9dba1f3820e1cf7790fda2
Saysamiche <140890694+Saysamiche@users.noreply.github.com>
2024-10-25 18:27:05 +0100

    created learn mode page

--- src/components/EdgeChair.jsx
+++ src/components/EdgeChair.jsx
@@ -1,5 +1,8 @@
+import "../css/EdgeStanding.css";
+
 export const EdgeChair = () => {
-    return <img src="" />
-}
+  return <img className="edge-chair" src="/assets/Chair 1.svg" />;
+};
 
 export default EdgeChair;
+
