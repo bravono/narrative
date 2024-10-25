a41f72b619aa03b15b5345c26681dfb88376b0ab
Saysamiche <140890694+Saysamiche@users.noreply.github.com>
2024-10-25 09:40:16 +0100

    Added the Lever  svgs and created the Component

---
+++ src/components/Lever.jsx
@@ -0,0 +1,14 @@
+export default function Lever() {
+  return (
+    <div>
+      <div className="lever-types">
+        <img src="/assets/unsorted.svg" alt="unsorted-svg" />
+        <img src="/assets/sorted.svg" alt="sorted-svg" />
+      </div>
+      
+      {/* <div className="sorted-lever">
+        <img src="/assets/Frame 135.svg" alt="sorted-svg" />
+      </div> */}
+    </div>
+  );
+}
