81218ff091f712509015917710a525dcf868b2c5
Mohammed Najeem <mohammednajeem18@gmail.com>
2024-10-25 18:04:50 +0100

    Add radio button

--- src/components/RadioButton.jsx
+++ src/components/RadioButton.jsx
@@ -1,16 +1,8 @@
-import { useState } from "react";
 import "../css/RadioButton.css";
 
 function RadioButton() {
-  const [isChecked, setiChecked] = useState(false);
   return (
     <>
-      <img
-        src="/assets/RadioButton.svg"
-        alt="radiobutton"
-        onClick={() => setIsChecked(!isChecked)}
-        className={isChecked ? "checked" : "unchecked"}
-      />
       <div className="radio-container">
         <input type="radio" />
       </div>
