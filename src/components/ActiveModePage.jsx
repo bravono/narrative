ca7727304feaace53f39ae285b8ac71e06132f2f
Ahbideen Yusuf <ahbideeny@gmail.com>
2024-11-02 12:56:27 +0100

    Remove MainContent component

--- src/components/ActiveModePage.jsx
+++ src/components/ActiveModePage.jsx
@@ -1,30 +1,48 @@
+import React from "react";
 import Header from "./Header";
-import MainContent from "./MainContent";
 import Footer from "./Footer";
 import RadioButton from "./RadioButton";
-// import Header from "../components/Header";
 import Queue from "./Queue";
 import Button from "./Button";
 import Ring from "./Ring";
 import LowerPointerArrow from "./LowerPointerArrow";
 import TalkBubble from "./TalkBubble";
 import EdgeStanding from "./EdgeStanding";
-// import LoveScroll from "../components/LoveScroll";
 import StickyArrow from "./StickyArrow";
 import ActiveBlank from "./ActiveBlank";
 import Percentage from "./Percentage";
 import ChoiceRapper from "./ChoiceRapper";
 import RingListContainer from "./RingListContainer";
 import Column from "./Column";
+import Barrel from "./Barrel";
+import MiddleButton from "./MiddleButton";
+import ScrollArrow from "./ScrollArrow";
+import Teleprompter from "./Telepromter";
+import "../css/mainContent.css";
 
 function ActiveModePage() {
+  const text = `
+  START hello my name is Najeem Mohammed , from Ilorin Kwara State hello my name is Njeem Mohammed , ,hello my name is Njeem Mohammed hello my name is Njeem Mohammed hello my name is Njeem Mohammed hello my name is Njeem Mohammed , from Ilorin Kwara State END`;
   return (
     <main className="main-container">
       <section className="top-section">
         <Header classNameA={"primary"} />
       </section>
       <section className="middle-section">
-        <MainContent classNameB={"primary"} />
+        <div className="body_content">
+          <div className="story_queue-group">
+            <ScrollArrow />
+            <Queue className={"queue question"}>
+              <Teleprompter text={text} />
+            </Queue>
+          </div>
+          <MiddleButton />
+          <div className="story_queue-single">
+            <Queue className={"queue answer"}>
+              <Barrel />
+            </Queue>
+          </div>
+        </div>
       </section>
       <section className="bottom-section">
         <Footer
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,33 +1,5 @@
-import ScrollArrow from "./ScrollArrow";
-import MiddleButton from "./MiddleButton";
-import Queue from "./Queue";
-import Teleprompter from "./Telepromter";
-import "../css/mainContent.css";
-import ItemList from "./ItemList";
-import StickyArrow from "./StickyArrow";
-import CheckBox from "./CheckBox";
-import CheckBoxDone from "./CheckBoxDone";
-import CheckBoxInactive from "./CheckBoxInactive";
-import Bar from "./Bar";
-import Barrel from "./Barrel";
-
-function MainContent({ ...props }) {
-  return (
-    <div className="body_content">
-      <div className="story_queue-group">
-        <ScrollArrow />
-        <Queue className={"queue question"}>
-          <Teleprompter text="START hello my name is Najeem Mohammed , from Ilorin Kwara State hello my name is Njeem Mohammed , ,hello my name is Njeem Mohammed hello my name is Njeem Mohammed hello my name is Njeem Mohammed hello my name is Njeem Mohammed , from Ilorin Kwara State END" />
-        </Queue>
-      </div>
-      <MiddleButton {...props} />
-      <div className="story_queue-single">
-        <Queue className={"queue answer"}>
-          <Bar />
-        </Queue>
-      </div>
-    </div>
-  );
+function MainContent({ children, ...props }) {
+  return;
 }
 
 export default MainContent;
