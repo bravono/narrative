8623e47f1d7184975a4ac51586f700d07838478a
Ahbideen Yusuf <ahbideeny@gmail.com>
2024-11-02 12:56:52 +0100

    Add a table overlay to barrel

--- src/components/Barrel.jsx
+++ src/components/Barrel.jsx
@@ -3,8 +3,84 @@ import "../css/Barrel.css";
 const Barrel = () => {
   return (
     <div className="barrel">
-      <img src="/assets/Barrel_skeleton.svg" />;
-      <img src="/assets/Column_dotted.svg" className="column_dotted" />
+      <table>
+        <thead>
+          <th>HEADING</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>3</td>
+            <td>3</td>
+            <td>3</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>4</td>
+            <td>4</td>
+            <td>4</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>5</td>
+            <td>5</td>
+            <td>5</td>
+            <td>5</td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>6</td>
+            <td>6</td>
+            <td>6</td>
+            <td>6</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>7</td>
+            <td>7</td>
+            <td>7</td>
+            <td>7</td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td>8</td>
+            <td>8</td>
+            <td>8</td>
+            <td>8</td>
+          </tr>
+          <tr>
+            <td>9</td>
+            <td>9</td>
+            <td>9</td>
+            <td>9</td>
+            <td>9</td>
+          </tr>
+          <tr>
+            <td>10</td>
+            <td>10</td>
+            <td>10</td>
+            <td>10</td>
+            <td>10</td>
+          </tr>
+        </tbody>
+      </table>
+      {/* <img src="/assets/Column_dotted.svg" className="column_dotted" /> */}
     </div>
   );
 };
