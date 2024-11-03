3acd57a122cc09afa1ad4d1d9160506c772fd5dd
Ahbideen Yusuf <ahbideeny@gmail.com>
2024-11-03 16:24:03 +0100

    Add the answer queue buttons

--- src/components/Barrel.jsx
+++ src/components/Barrel.jsx
@@ -1,86 +1,95 @@
 import React from "react";
+import AnswerQueueButtons from "./composed/AnswerQueueButtons";
+import Lever from "./Lever";
 import "../css/Barrel.css";
+
 const Barrel = () => {
   return (
-    <div className="barrel">
-      <table>
-        <thead>
-          <th>HEADING</th>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>2</td>
-            <td>2</td>
-            <td>2</td>
-            <td>2</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>3</td>
-            <td>3</td>
-            <td>3</td>
-            <td>3</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>4</td>
-            <td>4</td>
-            <td>4</td>
-            <td>4</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>5</td>
-            <td>5</td>
-            <td>5</td>
-            <td>5</td>
-          </tr>
-          <tr>
-            <td>6</td>
-            <td>6</td>
-            <td>6</td>
-            <td>6</td>
-            <td>6</td>
-          </tr>
-          <tr>
-            <td>7</td>
-            <td>7</td>
-            <td>7</td>
-            <td>7</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <td>8</td>
-            <td>8</td>
-            <td>8</td>
-            <td>8</td>
-            <td>8</td>
-          </tr>
-          <tr>
-            <td>9</td>
-            <td>9</td>
-            <td>9</td>
-            <td>9</td>
-            <td>9</td>
-          </tr>
-          <tr>
-            <td>10</td>
-            <td>10</td>
-            <td>10</td>
-            <td>10</td>
-            <td>10</td>
-          </tr>
-        </tbody>
-      </table>
-      {/* <img src="/assets/Column_dotted.svg" className="column_dotted" /> */}
+    <div className="barrel-set">
+      <Lever />
+      <div className="barrel">
+        <table>
+          <thead>
+            <th>HEADING HERE </th>
+            <th></th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>2</td>
+              <td>2</td>
+              <td>2</td>
+              <td>2</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>3</td>
+              <td>3</td>
+              <td>3</td>
+              <td>3</td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>4</td>
+              <td>4</td>
+              <td>4</td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>5</td>
+              <td>5</td>
+              <td>5</td>
+              <td>5</td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td>6</td>
+              <td>6</td>
+              <td>6</td>
+              <td>6</td>
+            </tr>
+            <tr>
+              <td>7</td>
+              <td>7</td>
+              <td>7</td>
+              <td>7</td>
+              <td>7</td>
+            </tr>
+            <tr>
+              <td>8</td>
+              <td>8</td>
+              <td>8</td>
+              <td>8</td>
+              <td>8</td>
+            </tr>
+            <tr>
+              <td>9</td>
+              <td>9</td>
+              <td>9</td>
+              <td>9</td>
+              <td>9</td>
+            </tr>
+            <tr>
+              <td>10</td>
+              <td>10</td>
+              <td>10</td>
+              <td>10</td>
+              <td>10</td>
+            </tr>
+          </tbody>
+        </table>
+        {/* <img src="/assets/Column_dotted.svg" className="column_dotted" /> */}
+      </div>
+      <p>Select Up to Six</p>
+      <AnswerQueueButtons />
     </div>
   );
 };
