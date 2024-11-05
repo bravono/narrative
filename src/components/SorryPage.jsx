f10672bede037211500b7969d3901ea20f30c435
Mohammed Najeem <mohammednajeem18@gmail.com>
2024-11-05 20:40:45 +0100

    Add sorrypage component

---
+++ src/components/SorryPage.jsx
@@ -0,0 +1,13 @@
+import Queue from "./Queue";
+
+function Sorry() {
+  return (
+    <main className="main-container">
+      <Queue className={"queue question"}>
+        <p>We are sorry to see you go, but "Goodbye"</p>
+      </Queue>
+    </main>
+  );
+}
+
+export default Sorry;
