ed188acdf6428c9c275c7cdeba0eb70e1bc2a207
Ahbideen Yusuf <ahbideeny@gmail.com>
2024-11-06 23:19:15 +0100

    Remove comment

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,6 @@
 import "../css/Button.css";
 
 const Button = ({ label, className, onClick }) => {
-  // const handleClick = (e) => {
-  //   console.log(
-  //     `${e.target.textContent} button clicked 😁😂😂 It is good to have some sun sha`
-  //   );
-  // };
   return (
     <button onClick={onClick} className={`button ${className}`}>
       {label}
