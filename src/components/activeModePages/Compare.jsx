68dffad5acbef01169f904e32cd38e4f0ac32eed
Ahbideen Yusuf <ahbideeny@gmail.com>
2024-11-07 17:13:25 +0100

    Add new pages

---
+++ src/components/activeModePages/Compare.jsx
@@ -0,0 +1,20 @@
+import Footer from "../Footer";
+import Queue from "../Queue";
+import Button from "../Button";
+import BottomButton from "../BottomButton";
+import FooterButton from "../FooterButton";
+
+function Compare() {
+  return (
+    <main className="main-container">
+      <Queue className={"queue story"}>
+        <p>COMING SOON</p>
+      </Queue>
+      <section className="bottom-section">
+        <FooterButton />
+      </section>
+    </main>
+  );
+}
+
+export default Compare;
