6f052a8da1c517471f498c85888c0afde9be7250
Ahbideen Yusuf <ahbideeny@gmail.com>
2024-11-09 17:45:34 +0100

    Dynamically render Barrel's table

--- src/components/composed/Barrel.jsx
+++ src/components/composed/Barrel.jsx
@@ -1,36 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AnswerQueueButtons from "./AnswerQueueButtons";
 import Lever from "../standalone/BarrelLever";
 import "../../css/Barrel.css";
 import StickyArrow from "../StickyArrow";
 
 const Barrel = ({ heading, choiceList, questionType, isFollowUP }) => {
+  const isFollowUp = true;
   const [isSorted, setIsSorted] = useState(false);
+  const [activeCell, setActiveCell] = useState(null);
 
   const handleSort = () => {
     setIsSorted((prevIsSorted) => !prevIsSorted);
     console.log(isSorted);
   };
+
+  const tableRows = choiceList.map((choice, index) => {
+    return (
+      <tr key={index}>
+        <td>{choice}</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    );
+  });
   return (
     <div className="barrel-set">
       <Lever sorted={isSorted} onClick={handleSort} />
       <StickyArrow className={"single_choice"} />
       <div className="barrel">
         <table>
-          <thead>
-            <th>{heading}</th>
-            <th></th>
-          </thead>
           <tbody>
-            {choiceList.map((item, index) => (
-              <tr key={index}>
-                <td>{item}</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-            ))}
+            <tr>
+              <th>{heading}</th>
+              <th>SINGLE CHOICE</th>
+            </tr>
+            {tableRows}
           </tbody>
         </table>
         {/* <img src="/assets/Column_dotted.svg" className="column_dotted" /> */}
