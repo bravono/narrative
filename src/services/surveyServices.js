da192895459939068cb03213b8bcb93854874f92
Ahbideen Yusuf <ahbideeny@gmail.com>
2024-11-28 10:52:47 +0100

    Connect to the backend for the first time

--- src/services/surveyServices.js
+++ src/services/surveyServices.js
@@ -2,7 +2,7 @@ import http from "./httpServices";
 import { apiEndpoint, testEndpoint } from "../config.json";
 
 export async function getSurvey() {
-  const survey = await http.get(testEndpoint);
+  const survey = await http.get(apiEndpoint);
 
   return survey;
 }
