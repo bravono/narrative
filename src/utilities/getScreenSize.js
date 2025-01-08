22242ef0505cad8ef2774e2cef56a27a9a0ca37a
Ahbideen Yusuf <ahbideeny@gmail.com>
2025-01-08 17:48:07 +0100

    Add getHeight utility

--- src/utilities/getScreenSize.js
+++ src/utilities/getScreenSize.js
@@ -1,10 +1,20 @@
 
-export function getScreenSize(size, stroke) {
+export function getScreenWidth(size, stroke) {
   const isMobile = window.innerWidth <= 500; 
 
   if (isMobile) {
     return 230;
   } else {
     return 200;
+  }
+}
+
+export function getScreenHeight() {
+  const isNotTall = window.innerHeight <= 740;
+
+  if (isNotTall) {
+    return 15;
+  } else {
+    return 20;
   }
 }
