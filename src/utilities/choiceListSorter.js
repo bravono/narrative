f4f712cd5e6b974d40772d3c07eda8afc40fd6e2
Ahbideen Yusuf <ahbideeny@gmail.com>
2025-01-12 22:43:12 +0100

    Prevent "others" from sorting

--- src/utilities/choiceListSorter.js
+++ src/utilities/choiceListSorter.js
@@ -1,5 +1,5 @@
 export function sortChoiceListByValue(choiceList, isDescending) {
-  const sortedList = [...choiceList].sort((a, b) => {
+  const sortedList = choiceList.sort((a, b) => {
     const valueA = Number(a.value);
     const valueB = Number(b.value);
     return isDescending ? valueB - valueA : valueA - valueB;
@@ -8,16 +8,26 @@ export function sortChoiceListByValue(choiceList, isDescending) {
 }
 
 export function sortChoiceListByName(choiceList, isDescending) {
-  const sortedList = [...choiceList].sort((a, b) => {
-    const nameA = a.name.toLowerCase(); 
-    const nameB = b.name.toLowerCase();
-    if (nameA < nameB) {
+  const sortedList = choiceList.sort((a, b) => {
+    const textA = a.text.toLowerCase();
+    const textB = b.text.toLowerCase();
+
+    // If either text is "others", prioritize it last
+    if (textA === "others") {
+      return 1; 
+    } else if (textB === "others") {
+      return -1; 
+    }
+
+    // Otherwise, sort alphabetically
+    if (textA < textB) {
       return isDescending ? 1 : -1; 
     }
-    if (nameA > nameB) {
-      return isDescending ? -1 : 1;
+    if (textA > textB) {
+      return isDescending ? -1 : 1; 
     }
-    return 0; // names are equal
+    return 0; // texts are equal
   });
   return sortedList;
 }
+
