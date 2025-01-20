9a67f14baf3d3f2f37234420d89dec9310d88692
Ahbideen Yusuf <ahbideeny@gmail.com>
2025-01-20 11:24:28 +0100

    Remove unwanted code

--- src/services/surveyServices.js
+++ src/services/surveyServices.js
@@ -1,6 +1,5 @@
 import http from "./httpServices";
 import { apiEndpoint, localEndpoint, remoteEndpoint } from "../config.json";
-import { apiEndpoint, localEndpoint, remoteEndpoint } from "../config.json";
 
 export async function getSurvey(session) {
   const survey = await http.get(remoteEndpoint);
