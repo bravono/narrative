99adcf9dc20ddc879c41613281e62b3c2b40e8a2
Ahbideen Yusuf <ahbideeny@gmail.com>
2025-02-04 12:41:32 +0100

    Remove unwanted code

--- src/components/standalone/Warning.jsx
+++ src/components/standalone/Warning.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import "../../css/warning.css";
-import { use } from "react";
 
 export default function Warning({ message, style }) {
   useEffect(() => {}, []);
@@ -10,6 +9,6 @@ export default function Warning({ message, style }) {
   return (ringTotal < 100 && ringTotal > 94) || ringTotal > 100 ? (
     <div className={style}>{message}</div>
   ) : (
-    <div className={style}>{message}</div>
+    ""
   );
 }
